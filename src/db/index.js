97eeeb6dad4b28381c68658102692d1809e97a66
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-03-20 18:02:10 +0500

    major utils setup

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `\n MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`
+      `MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("MongoDB Connection Error ", error);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-const app = express();
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at Port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed !!! ", error);
+  });
 
 /*
 // Simple But Non Cleaner Approach For DB
